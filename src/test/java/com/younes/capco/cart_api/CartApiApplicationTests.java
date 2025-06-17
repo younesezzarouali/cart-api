b88f54e4c90fb2b3b7f04b55e0d979825363a4b7
younes_ezzarouali <youness.ezzarouali@gmail.com>
2025-06-17 19:38:47 +0200

    initial commit

---
+++ src/test/java/com/younes/capco/cart_api/CartApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.younes.capco.cart_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
